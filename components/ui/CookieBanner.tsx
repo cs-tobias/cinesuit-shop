d95e75103025ac9101831db88dfd5b2757c20209
“cs-tobias” <tobias@cinesuit.com>
2024-02-19 08:45:54 +0100

    cookies-fix-2

--- components/ui/CookieBanner.tsx
+++ components/ui/CookieBanner.tsx
@@ -1,22 +1,28 @@
-// CookieBanner.tsx
 import React, { useEffect, useState } from "react";
 import Button from "../Button"; // Adjust path as necessary
 import { useConsent } from "../contexts/ConsentContext"; // Adjust path as necessary
 import { loadGA } from "@/utils/gtag"; // Ensure this path matches your project structure
 
 const CookieBanner = () => {
-  // Initialize isVisible to true by default, then set it based on localStorage in useEffect
-  const [isVisible, setIsVisible] = useState(true);
+  const [isVisible, setIsVisible] = useState(false); // Start with false to avoid flash
   const { setConsentGiven, setShowBanner } = useConsent();
 
   useEffect(() => {
-    const consent = localStorage.getItem("ga_consent");
-    const shouldShowBanner = !(consent === "granted" || consent === "denied");
-    setIsVisible(shouldShowBanner);
-    setShowBanner(shouldShowBanner);
-    if (consent) {
-      setConsentGiven(consent === "granted");
-    }
+    // Introduce a delay before showing the banner
+    const delay = 100; // Delay in milliseconds
+    const timer = setTimeout(() => {
+      const consent = localStorage.getItem("ga_consent");
+      const shouldShowBanner = !(consent === "granted" || consent === "denied");
+      setIsVisible(shouldShowBanner);
+      setShowBanner(shouldShowBanner);
+      if (consent === "granted") {
+        setConsentGiven(true);
+        loadGA(); // Load Google Analytics if consent was already given
+      }
+    }, delay);
+
+    // Cleanup function to clear the timeout if the component unmounts
+    return () => clearTimeout(timer);
   }, [setConsentGiven, setShowBanner]);
 
   const handleAccept = (): void => {
@@ -24,7 +30,7 @@ const CookieBanner = () => {
     setIsVisible(false);
     setConsentGiven(true);
     setShowBanner(false);
-    loadGA(); // Consider moving this call to a more appropriate place if needed
+    loadGA(); // Initialize Google Analytics now that consent has been given
   };
 
   const handleDeny = (): void => {
@@ -37,12 +43,17 @@ const CookieBanner = () => {
   if (!isVisible) return null;
 
   return (
-    <div className="flex flex-col justify-between bottom-4 left-4 fixed w-[380px] border-[1px] backdrop-blur-md bg-black bg-opacity-65 border-neutral-400 border-opacity-30 z-50 rounded-3xl p-5">
+    <div
+      className={`flex flex-col justify-between bottom-4 md:left-4 fixed md:w-[380px] border-[1px] backdrop-blur-md bg-black bg-opacity-65 border-neutral-400 border-opacity-30 z-50 rounded-3xl p-5 ${
+        isVisible ? "animate-fadeIn" : ""
+      }`}
+    >
       <h1 className="text-sm text-neutral-200 mb-4">
         This site uses tracking technologies. You may opt in or opt out of the
         use of these technologies.
       </h1>
       <div className="flex justify-between items-center">
+        {/* Consent Settings Button - Consider implementing its functionality */}
         <Button
           onClick={() => {}}
           className="text-white text-sm bg-black bg-opacity-0 border-[1px] border-neutral-400 border-opacity-30 hover:bg-opacity-30 transition-colors duration-300"
@@ -52,13 +63,13 @@ const CookieBanner = () => {
         <div className="flex gap-2">
           <Button
             onClick={handleDeny}
-            className="text-black text-sm bg-neutral-200 hover:bg-neutral-50 transition-colors duration-300"
+            className="text-black text-sm bg-neutral-300 hover:bg-neutral-50 transition-colors duration-300"
           >
             Deny
           </Button>
           <Button
             onClick={handleAccept}
-            className="text-black text-sm bg-neutral-200 hover:bg-neutral-50 transition-colors duration-300"
+            className="text-black text-sm bg-neutral-300 hover:bg-neutral-50 transition-colors duration-300"
           >
             Accept All
           </Button>
@@ -67,4 +78,5 @@ const CookieBanner = () => {
     </div>
   );
 };
+
 export default CookieBanner;
